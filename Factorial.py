5a6072ae33a1f0043dd45fe8d8c5e73b94aa61fb
rdPat <rdp1018ece@gmail.com>
2024-11-13 23:11:17 +0530

    commiting new

---
+++ Factorial.py
@@ -0,0 +1,20 @@
+
+
+def fact1(x):
+    fact=1
+    while(x>0):
+        fact=fact*x
+        x=x-1
+    print(fact)
+
+fact1(10)
+
+
+
+
+
+
+
+
+
+
---
+++ Function.py
@@ -0,0 +1,20 @@
+
+def square(x):
+    print("square of no :",x**2)
+
+
+square()
+
+
+#in python it will override to latest function
+def func():
+    print("Hello World0")
+def func():
+    print("Hello World1")
+def func():
+    print("Hello World2")
+
+func()
+
+
+
